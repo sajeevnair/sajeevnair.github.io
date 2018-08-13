3611570f040a4d5d829584bcc23b2567743ca3a6
Sajeev Nair <sajeev.nair174@gmail.com>
2018-08-13 13:01:25 -0700

    scroll position adjust

--- resources/js/script.js
+++ resources/js/script.js
@@ -28,7 +28,7 @@ $(document).ready(
                 // Using jQuery's animate() method to add smooth page scroll
                 // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
                 $('html, body').animate({
-                    scrollTop: $(hash).offset().top - 75
+                    scrollTop: $(hash).offset().top
                 }, 1000, function () {
 
                     // Add hash (#) to URL when done scrolling (default click behavior)

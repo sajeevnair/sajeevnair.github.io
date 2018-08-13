57543ef1b0ed0ab85e4dff5821d424f02235bfca
Sajeev Nair <sajeev.nair174@gmail.com>
2018-08-13 01:15:23 -0700

    delete repeated, js offset

--- resources/js/script.js
+++ resources/js/script.js
@@ -28,8 +28,8 @@ $(document).ready(
                 // Using jQuery's animate() method to add smooth page scroll
                 // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
                 $('html, body').animate({
-                    scrollTop: $(hash).offset().top
-                }, 800, function () {
+                    scrollTop: $(hash).offset().top - 75
+                }, 1000, function () {
 
                     // Add hash (#) to URL when done scrolling (default click behavior)
                     window.location.hash = hash;

a64b6dfe7bca1249c1ad09f8133b0da91fbb0b4b
Sajeev Nair <sajeev.nair174@gmail.com>
2018-08-13 02:59:24 -0700

    mobile nav, formatting

--- resources/js/script.js
+++ resources/js/script.js
@@ -37,4 +37,22 @@ $(document).ready(
             } // End if
         });
 
+        /*  Mobile Nav */
+
+        $('.js--nav-icon').click(function () {
+            var nav = $('.js--main-nav');
+            var icon = $('.js--nav-icon i');
+
+            nav.slideToggle(200);
+
+            if (icon.hasClass('fa-bars')) {
+                icon.addClass('fa-window-close');
+                icon.removeClass('fa-bars');
+            } else {
+                icon.addClass('fa-bars');
+                icon.removeClass('fa-window-close');
+            }
+
+        });
+
     });
